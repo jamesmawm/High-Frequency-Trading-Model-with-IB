dea25d446341c491a057642cfc3c83964cad2df6
Joseph Loss <28772631+chicago-joe@users.noreply.github.com>
2019-05-30 00:27:22 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-"""
-Author: James Ma
-Email stuff here: jamesmawm@gmail.com
-"""
-from models.hft_model import HFTModel
+# IE598 Project - Joseph Loss
+# An algorithmic trading trading strategy for IBroker's Python API.
+#
+# Original Templates, Classes, and Parameters located at:
+# https://github.com/jamesmawm/High-Frequency-Trading-Model-with-IB
+#
+from tmp_hft_model import HFTModel
 
 if __name__ == "__main__":
-    model = HFTModel(host='localhost',
-                     port=7496,
-                     client_id=101,
-                     is_use_gateway=False,
-                     evaluation_time_secs=20,
-                     resample_interval_secs='30s')
-    model.start(["JPM", "C"], 100)
+    model = HFTModel(host = 'localhost',
+                     # port = 4002,
+                     port = 7497,
+                     client_id = 1,
+                     is_use_gateway = False,
+                     evaluation_time_secs = 15,
+                     resample_interval_secs = '30s')
+
+    # model.start(["RDS A", "BP"], 250)
+    # model.start(["JPM", "BAC"], 100)
+    model.start(["SPY", "QQQ"], 100)
+
