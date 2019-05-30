8bf154fd2daa32a0f82b07c32ae81b11f1c92803
Joseph Loss <28772631+chicago-joe@users.noreply.github.com>
2019-05-30 00:25:12 -0500

    Add files via upload

--- classes/ib_util.py
+++ classes/ib_util.py
@@ -1,10 +1,6 @@
-"""
-Author: James Ma
-Email stuff here: jamesmawm@gmail.com
-"""
 from ib.ext.Contract import Contract
 from ib.ext.Order import Order
-import params.ib_data_types as DataType
+import ib_data_types as DataType
 
 
 class IBUtil:
@@ -16,6 +12,18 @@ class IBUtil:
         contract_tuple = (stock, 'STK', 'SMART', 'USD', '', 0.0, '')
         stock_contract = self.__make_ib_contract(contract_tuple)
         return stock_contract
+
+
+    def create_stock_order(self, quantity, is_buy, is_market_order=True):
+        order = Order()
+        order.m_totalQuantity = quantity
+        order.m_orderType = \
+            DataType.ORDER_TYPE_MARKET if is_market_order else \
+            DataType.ORDER_TYPE_LIMIT
+        order.m_action = \
+            DataType.ORDER_ACTION_BUY if is_buy else \
+            DataType.ORDER_ACTION_SELL
+        return order
 
     @staticmethod
     def __make_ib_contract(contract_tuple):
@@ -29,13 +37,20 @@ class IBUtil:
         new_contract.m_right = contract_tuple[6]
         return new_contract
 
-    def create_stock_order(self, quantity, is_buy, is_market_order=True):
+    @staticmethod
+    def create_generic_contract(symbol, sec_type, exch, prim_exch, curr):
+        contract = Contract()
+        contract.m_symbol = symbol
+        contract.m_secType = sec_type
+        contract.m_exchange = exch
+        contract.m_primaryExch = prim_exch
+        contract.m_currency = curr
+        return contract
+
+    @staticmethod
+    def create_generic_order(order_type, quantity, action):
         order = Order()
+        order.m_orderType = order_type
         order.m_totalQuantity = quantity
-        order.m_orderType = \
-            DataType.ORDER_TYPE_MARKET if is_market_order else \
-            DataType.ORDER_TYPE_LIMIT
-        order.m_action = \
-            DataType.ORDER_ACTION_BUY if is_buy else \
-            DataType.ORDER_ACTION_SELL
+        order.m_action = action
         return order
